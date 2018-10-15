2aeeff3205ff28a6b4ff5cfc401aa5f369a62a42
Jonas Cardoso <jonascard60@gmail.com>
2018-10-15 14:12:46 -0300

    fix for bootanimation

--- common/install.sh
+++ common/install.sh
@@ -1,1 +1,6 @@
-#placeholder
+if [ $MAGISK_VER_CODE -lt 1640 ]; 
+then 
+	cp_ch $INSTALLER/common/bootanimation.zip /cache/magisk_mount/system/media/bootanimation.zip 
+else 
+	cp_ch $INSTALLER/common/bootanimation.zip /data/adb/magisk_simple/system/media/bootanimation.zip 
+fi
