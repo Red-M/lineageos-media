36a4e04b3b572de2235ed095dc3c205970bace30
Red_M <1468433+Red-M@users.noreply.github.com>
2018-10-31 23:18:58 +1000

    line endings

--- common/uninstall.sh
+++ common/uninstall.sh
@@ -1,1 +1,9 @@
-if [ $MAGISK_VER_CODE -lt 1640 ]; then    base_folder=/cache/magisk_mountelse 	base_folder=/data/adb/magisk_simplefirm $base_folder/system/media/bootanimation.ziprm $base_folder/system/media 2> /dev/nullrm $base_folder/system 2> /dev/null
+if [ $MAGISK_VER_CODE -lt 1640 ]; 
+then
+    base_folder=/cache/magisk_mount
+else 
+	base_folder=/data/adb/magisk_simple
+fi
+rm $base_folder/system/media/bootanimation.zip
+rm $base_folder/system/media 2> /dev/null
+rm $base_folder/system 2> /dev/null
